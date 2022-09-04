524806efb5cc475fd887b59a04b781982df87c01
yumihong <ghddid13@naver.com>
2022-09-04 17:20:48 +0900

    ref기능추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import MyComponent from "./MyComponent";
-import Say from "./Say";
+// import MyComponent from "./MyComponent";
+// import Say from "./Say";
+// import EventPractice from "./EventPractice";
 
-const App = () => {
-  return <Say />;
+// const App = () => {
+//   return <EventPractice />;
+// }
+
+import React, { Component } from 'react';
+// import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref)=>this.ScrollBox=ref}/>
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
