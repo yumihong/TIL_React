8d51f7936e35b8b50c53c786d0730028aba94e72
yumihong <ghddid13@naver.com>
2022-09-07 22:17:02 +0900

    map, key, filter 사용해보기

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@
 // }
 
 import React, { Component } from 'react';
+import IterationSample from './IterationSample';
 // import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+// import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref)=>this.ScrollBox=ref}/>
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+        <IterationSample />
       </div>
       
     );
